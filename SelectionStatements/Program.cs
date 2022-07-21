bc2fa090f138df70cbe7454b83078b6a633de9b7
TylerN93 <tylernewton73@gmail.com>
2022-07-21 13:58:58 -0500

    initial commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+            Console.WriteLine("Lets play a game");
+            
+            Console.WriteLine("What it the top number limit");
+
+            var limit = int.Parse(Console.ReadLine());
+
+            var random = new Random();
+
+            var number = random.Next(1, limit);
+
+            Console.WriteLine("Guess a number");
+
+            var guess = int.Parse(Console.ReadLine());
+
+
+
+            if (guess <= number)
+            { Console.WriteLine("Too Low");
+            }
+
+            else if (guess >= number)
+            {  Console.WriteLine("Too High"); }
+
+            else
+            { Console.WriteLine("Nevermind"); }
+
+            
+
+
+            
+
+
+        }
+    }
+}
